6c54b24d9272d0a095a96adb1789fa51ce8f9a12
erdemtopal <erdemtopals@gmail.com>
2022-06-10 13:11:03 +0300

    make it 6

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+
+rails 6 ve ruby 2,7 ile ilerliyoruz.  
+
+önce https://www.youtube.com/watch?v=J5g3CGXyn7k  'den bootstrap 5 yükledim.  
+
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,17 @@
+module ApplicationHelper
+	def resource_name
+		:admin
+	end
+
+	def resource
+		@resource ||= Admin.new
+	end
+
+	def resource_class
+		Admin
+	end
+
+	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:admin]
+	end
+end
