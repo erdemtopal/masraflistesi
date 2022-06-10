2aa348009bfe59da681f995fbadfde2fd496c41b
erdemtopal <erdemtopals@gmail.com>
2022-06-10 13:48:01 +0300

    make it ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   get 'dashboard/index'
   get 'home/index'
+  get 'home/underconstruction'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
